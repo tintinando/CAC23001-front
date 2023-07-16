0ae9f5aac4c62b2dff5892113acc7f8d50adad76
Martin Andonegui <107717999+tintinando@users.noreply.github.com>
2023-07-16 02:05:32 -0300

    Modifico API

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,4 +1,4 @@
-const apiURL = 'http://127.0.0.1:5000';
-// const apiURL = 'https://tintinando.pythonanywhere.com';
+// const apiURL = 'http://127.0.0.1:5000';
+const apiURL = 'https://tintinando.pythonanywhere.com';
 
 export default apiURL;
